44f22a1d8f8194202842b6b0e5c50a0dd8bc18b3
dravidsz <hdavidortega32@gmail.com>
2024-12-29 09:28:08 -0500

    creacion de hover para la barra de navegacion

--- webComponets/header.js
+++ webComponets/header.js
@@ -20,10 +20,10 @@ class Header extends HTMLElement {
                   <h2 class="logo-text">D R A V I D Z <span class="logo-dot">.</span></h2>
                 </div>
                 <div class="nav-links">
-                  <a href="index.html" class="nav-link">Inicio</a>
-                  <a href="trabajos.html" class="nav-link">Trabajo</a>
-                  <a href="habilidades.html" class="nav-link">Habilidades</a>
-                  <a href="contacto.html" class="nav-link">Contacto</a>
+                  <a href="index.html" class="nav-link-a">Inicio</a>
+                  <a href="trabajos.html" class="nav-link-a">Trabajo</a>
+                  <a href="habilidades.html" class="nav-link-a">Habilidades</a>
+                  <a href="contacto.html" class="nav-link-a">Contacto</a>
                 </div>
                 <button class="btn-whatsapp">
                   <a href="https://wa.me/+50762969217" target="_blank">
